1be2287f29b79b8259c8b1e3ddfde095d0ce1626
crypt0n <61395407+crypt0nX@users.noreply.github.com>
2025-12-16 05:05:30 +0800

    Log SoC selection from config

--- make/config.mk
+++ make/config.mk
@@ -1,6 +1,15 @@
 include $(PRJ_PATH)/config
 -include $(SYS_PATH)/include/config/auto.conf
 
+# Print the resolved SoC from config for easier build-time inspection
+ifeq ($(origin SoC), undefined)
+  $(info SSDK CONFIG: SoC is not set)
+else
+  $(info SSDK CONFIG: SoC=$(SoC))
+endif
+
+-include $(SYS_PATH)/include/config/auto.conf
+
 ifndef SYS_PATH
   $(error SYS_PATH isn't defined!)
 endif
